5e7a3fa9bc499247923edc3ee1c31a0f9817dec5
Mreenal Singh <mreenal.singh@hotmail.com>
2023-09-11 15:46:49 +0200

    Extentsreport added with screenshot feature

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -1,14 +1,21 @@
 package com.qa.opencart.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverFactory {
@@ -123,6 +130,36 @@ public class DriverFactory {
 		
 		return prop;
 		
+	}
+	
+	/**
+	 * take screenshot 
+	 * @return 
+	 */
+	
+	public static String getScreenshot() {
+		File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+		String screenshotDir = "./screenshot/";
+		String path = System.getProperty("user.dir")+"/screenshot/"+System.currentTimeMillis()+".png";
+		File destination = new File(path);
+		
+		Path sshotpath = Paths.get(screenshotDir);
+		// if directory exists?
+		if (!Files.exists(sshotpath)) {
+			try {
+				Files.createDirectories(sshotpath);
+			} catch (IOException e) {
+				// fail to create directory
+				e.printStackTrace();
+			}
+		}
+			
+		try {
+			FileHandler.copy(srcFile, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
 	}
 
 }
